952abbde483e1923c4acde0f2e93a30b8554b7a2
jbyuki <jyburkhard@hotmail.com>
2021-11-23 21:47:52 +0100

    update vision_utils

--- vision/vision_utils.py
+++ vision/vision_utils.py
@@ -19,41 +19,70 @@ corner_ids = {
     6: 2,
 }
 
-def localize_thymio(img):
-  # Detect aruco
-  (corners, ids, rejected) = cv.aruco.detectMarkers(img, arucoDict,
+# Ids
+thymio_id = 2
+
+# Detect all AruCo in image
+def detect_aruco(img):
+  detected = cv.aruco.detectMarkers(img, arucoDict,
     parameters=arucoParams)
+  return detected 
 
+# Get position of ArUco marker
+def get_pos_aruco(img, detected, search_id):
+  (corners, ids, rejected) = detected
 
-  if len(corners) == 1:
-    c = np.int32(corners[0][0])
-  else:
+  if ids is not None:
+    for i, id in enumerate(ids):
+      c = corners[i][0]
+      if id[0] == search_id:
+        center = (c[0]+c[1]+c[2]+c[3])/4
+        return center, c
+  return (None, None)
+
+# Given the ID, gives the position, angle of the Thymio
+# assuming an ArUco is put on top of it
+def localize_thymio(img, detected):
+  # Detect aruco
+  (center, c) = get_pos_aruco(img, detected, thymio_id)
+
+  if not center:
     return (None, None, None)
-
-
-  # Compute center
-  center = (c[0]+c[1]+c[2]+c[3])//4
 
   # c[0]        TOP LEFT
   # c[1]        BOTTOM RIGHT
   # c[2]        BOTTOM LEFT
   # c[3]        TOP LEFT
   # Compute orientation
-  top_middle = (c[0]+c[3])//2
+  top_middle = (c[0]+c[3])/2
   dir = top_middle - center
   angle = np.arctan2(dir[0], dir[1])
 
   return (c, center, angle)
 
+# Replace all detected AruCo markers with
+# white square.
+# img must be grayscale.
+def erase_aruco(img, detected):
+  (corners, _, _) = detected
 
+  if corners is not None:
+    for i in range(len(corners)):
+      c = corners[i][0]
+
+      # Remove AruCo
+      cv.drawContours(img, [c], 0, 255, -1)
+
+
+# Gives a binary grid representation of 
+# the labyrinth
+# img must be grayscale
 def detect_labyrinth(img, wall_size):
   h,w = img.shape
 
-  # Localize
-  (c, center, angle) = localize_thymio(img)
-
   # Remove AruCo
-  cv.drawContours(img, [c], 0, 255, -1)
+  detected = detect_aruco(img)
+  erase_aruco(img, detected)
 
   # Threshold
   _, th = cv.threshold(img,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
@@ -78,49 +107,32 @@ def detect_labyrinth(img, wall_size):
   center[0] = int(np.round(center[0]*desired_w/w))
   center[1] = int(np.round(center[1]*desired_h/h))
 
-  return th, c, center, angle
+  return th
 
-def detect_aruco(img):
-  return cv.aruco.detectMarkers(img, arucoDict,
-    parameters=arucoParams)
-
+# Get perspective transform from img from ArUco corners
+# Returns none if corners are not detected
 def get_labyrinth_perspective(img):
-  (corners, ids, rejected) = cv.aruco.detectMarkers(img, arucoDict,
-    parameters=arucoParams)
+  detected = detect_aruco(img)
 
-  valid_corners = 0
   cs = [None]*4
+  for (id, idx) in corner_ids.items():
+    (center, _) = get_pos_aruco(img, detected, id)
+    if center is not None:
+      cs[idx] = center
+    else:
+      return None
 
-  # Get corners positions
-  if ids is not None:
-    for i, id in enumerate(ids):
-      c = corners[i][0]
-      id = id[0]
-      center = (c[0]+c[1]+c[2]+c[3])/4
+  # Do perspective correction
+  pts1 = np.array([cs[0], cs[1], cs[3], cs[2]])
+  pts2 = np.float32([[res_h,res_w], [0, res_w], [res_h, 0], [0, 0]])
 
-      if id in corner_ids:
-        idx = corner_ids[id]
-        cs[idx] = center
-        valid_corners += 1
+  return cv.getPerspectiveTransform(pts1,pts2)
 
-  # If 4 corners, crop
-  if valid_corners == 4:
-
-    # Do perspective correction
-    pts1 = np.array([cs[0], cs[1], cs[3], cs[2]])
-    pts2 = np.float32([[res_h,res_w], [0, res_w], [res_h, 0], [0, 0]])
-
-    M = cv.getPerspectiveTransform(pts1,pts2)
-
-    # for i in range(4):
-      # cv.line(img, np.int32(cs[i]), np.int32(cs[(i+1)%4]), (0, 255, 0), 2)
-    return M
-  else:
-    return None
-
+# Given perspective transform, crops the original image
 def crop_labyrinth(img, M):
     return cv.warpPerspective(img,M,(res_h,res_w))
 
+# Utility function to wait for corner calibration
 def calibrate_corners(cam):
   while True:
     ret_val, img = cam.read()
@@ -134,4 +146,3 @@ def calibrate_corners(cam):
 
     if cv.waitKey(1) == 27: 
       return None
-
